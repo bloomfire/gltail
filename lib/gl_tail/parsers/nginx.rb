6335cac31dd0f6b2f67290fded8d0e163470d6f7
David McCullars <dmccullars@bloomfire.com>
2014-05-02 14:26:49 -0500

    Use new hosts field instead of referrer

--- lib/gl_tail/parsers/nginx.rb
+++ lib/gl_tail/parsers/nginx.rb
@@ -7,7 +7,7 @@
 # Parser which handles nginx logs
 class NginxParser < Parser
   def parse( line )
-    _, remote_addr, remote_user, request, status, size, referrer, http_user_agent, http_x_forwarded_for, host = /^([^\s]+) - ([^\s]+) \[.*\] "([^\"]*)" (\d+) (\d+) \"([^\"]*)\" \"(.*)\" "(.*)\" (\w+)/.match(line).to_a
+    _, remote_addr, remote_user, request, status, size, referrer, http_user_agent, http_x_forwarded_for, host = /^([^\s]+) - ([^\s]+) \[.*\] "([^\"]*)" (\d+) (\d+) \"([^\"]*)\" \"(.*)\" "(.*)\" (\S+)/.match(line).to_a
     #_, remote_addr, remote_user, request, status, size, referrer, http_user_agent, http_x_forwarded_for = /^([^\s]+) - ([^\s]+) \[.*\] (\d+) \"(.+)\" (\d+) \"(.*)\" \"([^\"]*)\" \"(.*)\"/.match(line).to_a
 
     if request && request != '-'
