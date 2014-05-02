63343fbac6a00a21cee2273d77825388ef6686b3
David McCullars <dmccullars@bloomfire.com>
2014-05-02 14:19:14 -0500

    Use new hosts field instead of referrer

--- lib/gl_tail/parsers/nginx.rb
+++ lib/gl_tail/parsers/nginx.rb
@@ -7,11 +7,10 @@
 # Parser which handles nginx logs
 class NginxParser < Parser
   def parse( line )
-    _, remote_addr, remote_user, request, status, size, referrer, http_user_agent, http_x_forwarded_for = /^([^\s]+) - ([^\s]+) \[.*\] "([^\"]*)" (\d+) (\d+) \"([^\"]*)\" \"(.*)\" "(.*)\"/.match(line).to_a
+    _, remote_addr, remote_user, request, status, size, referrer, http_user_agent, http_x_forwarded_for, host = /^([^\s]+) - ([^\s]+) \[.*\] "([^\"]*)" (\d+) (\d+) \"([^\"]*)\" \"(.*)\" "(.*)\" (\w+)/.match(line).to_a
     #_, remote_addr, remote_user, request, status, size, referrer, http_user_agent, http_x_forwarded_for = /^([^\s]+) - ([^\s]+) \[.*\] (\d+) \"(.+)\" (\d+) \"(.*)\" \"([^\"]*)\" \"(.*)\"/.match(line).to_a
 
     if request && request != '-'
-      referrer_host = referrer.to_s[/^http[s]?:\/\/([^\/]+)/, 1] || http_x_forwarded_for
       method, full_url, _ = request.split(' ')
       url, parameters = String(full_url).split('?')
       url ||= ''
@@ -19,7 +18,7 @@ class NginxParser < Parser
       add_activity(:block => 'sites', :name => server.name, :size => size.to_i)
       add_activity(:block => 'urls', :name => url) unless url =~ %r{^/uptime\.txt|^/ckeditor/|^/images/|^/assets/}
       #add_activity(:block => 'users', :name => remote_addr, :size => size.to_i)
-      add_activity(:block => 'referrers', :name => referrer_host.sub('https://', '').sub('.bloomfire.com','')) unless referrer_host.nil? || referrer_host.include?(server.name) || referrer_host.include?(server.host) || referrer_host == '-'
+      add_activity(:block => 'hosts', :name => host.sub('.bloomfire.com', '')) unless host.nil? || host == '-'
       #add_activity(:block => 'user agents', :name => http_user_agent, :type => 3) unless http_user_agent.nil?
 
       if( url.include?('.gif') || url.include?('.jpg') || url.include?('.png') || url.include?('.ico'))
@@ -41,14 +40,13 @@ class NginxParser < Parser
       add_activity(:block => 'content', :name => type)
       add_activity(:block => 'status', :name => status, :type => 3)
 
-      subdomain = String(referrer_host).sub('.bloomfire.com', '')
-      add_event(:block => 'info', :name => "Course Published", :message => "Course Published: #{subdomain}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/post')
-      add_event(:block => 'info', :name => "Series Published", :message => "Series Published: #{subdomain}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/series')
-      add_event(:block => 'info', :name => "Question Answered", :message => "Question Answered: #{subdomain}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/answers')
-      add_event(:block => 'info', :name => "Question Asked", :message => "Question Asked: #{subdomain}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/questions')
-      add_event(:block => 'info', :name => "Comment", :message => "Comment: #{subdomain}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/comments')
-      add_event(:block => 'info', :name => "Logins", :message => "Login: #{subdomain}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/login')
-      add_event(:block => 'info', :name => "Registration", :message => "Register: #{subdomain}", :update_stats => true, :color => [1.5, 0.0, 0.0, 1.0]) if method == "POST" && url.include?('/register')
+      add_event(:block => 'info', :name => "Course Published", :message => "Course Published: #{host}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/post')
+      add_event(:block => 'info', :name => "Series Published", :message => "Series Published: #{host}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/series')
+      add_event(:block => 'info', :name => "Question Answered", :message => "Question Answered: #{host}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/answers')
+      add_event(:block => 'info', :name => "Question Asked", :message => "Question Asked: #{host}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/questions')
+      add_event(:block => 'info', :name => "Comment", :message => "Comment: #{host}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/comments')
+      add_event(:block => 'info', :name => "Logins", :message => "Login: #{host}", :update_stats => true, :color => [1.5, 1.0, 0.5, 1.0]) if method == "POST" && url.include?('/login')
+      add_event(:block => 'info', :name => "Registration", :message => "Register: #{host}", :update_stats => true, :color => [1.5, 0.0, 0.0, 1.0]) if method == "POST" && url.include?('/register')
     end
   end
 end
