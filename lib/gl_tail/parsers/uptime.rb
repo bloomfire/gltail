203f76eedbefa1ac4711d33706be37538c62d35d
David McCullars <david@bloomfire.com>
2019-05-08 20:36:58 -0500

    Change uptime parser to get cpu count

--- lib/gl_tail/parsers/uptime.rb
+++ lib/gl_tail/parsers/uptime.rb
@@ -6,13 +6,15 @@
 
 # Parser which handles uptime reports from while [ 1 ]; do echo -n ${PS1/ \\[*/} ' ' && cat /proc/loadavg; sleep 5; done;
 class UptimeParser < Parser
+  BAD = [1.0, 0.0, 0.0, 1.0]
+  GOOD = [0.0, 1.0, 0.0, 1.0]
   def parse( line )
-    _, server, loadavg = /^([^\s]+)\s+([^\s]+)/.match(line).to_a
-    if server && loadavg
-      cpuload = loadavg.to_f
-      bad = [1.0, 0.0, 0.0, 1.0]
-      good = [0.0, 1.0, 0.0, 1.0]
-      color = cpuload > 3 ? bad : good
+    server, cpu_count, cpuload, _ = line.split(/\s+/, 4)
+    if server && cpu_count && cpuload
+      server.sub!(/^production-/, '') # FIXME -- should support other environments
+      unsafe_load = cpu_count.to_f * 0.75
+      cpuload = cpuload.to_f
+      color = cpuload > unsafe_load ? BAD : GOOD
       add_activity(:block => 'load', :name => server, :real_size => cpuload, :color => color, :type => 3) # 3=noblob?
     end
   end
